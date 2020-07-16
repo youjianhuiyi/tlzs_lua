90cfb139992b77d3fc858dc41f40f8f20074bbd7
罗露 <luolu@qq.com>
2020-07-16 16:24:30 +0800

    7-16 fix file

--- GM/script/user_define/仓库存钱.lua
+++
@@ -1,6 +0,0 @@
-
--- 这里可以修改包内留下多少铜
-local nTong = 0
-
-Bank_SaveMoney(nTong)
-
--- GM/script/user_define/商会取钱.lua
+++
@@ -1,5 +0,0 @@
-PushDebugMessage("前往商会取钱,修改取钱数量请在界面点编辑后保存")
-
-local nNeedMoney = 5 * 10000 -- 默认取5J,单位是铜 自己可修改
-
-PS_GetMoney(nNeedMoney)
