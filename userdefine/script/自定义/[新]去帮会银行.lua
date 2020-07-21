7b76877e5eed35009132073483b04f44458570b2
yulinzhihou <luolu@qq.com>
2020-07-21 16:13:10 +0800

    7-21 优化调整去帮派脚本

--- userdefine/script/自定义/[新]去帮会银行.lua
+++ userdefine/script/自定义/[新]去帮会银行.lua
@@ -1,8 +1,17 @@
+--[[
+    脚本功能：直接冲到帮会银行去
+	author:yulinzhihou
+	email:yulinzhihou@gmail.com
+	github:https://github.com/yulinzhihou
+	Date:2020-07-21
+]]
+
 LoadScript("LtoGuild.lua")
 QuestFrameOptionClicked("进入本帮城市");Sleep(3000)
 PushDebugMessage("#eDC4C18#cFFFF00需要手动点击升级。本任务只是方便多号玩家操作，省去取物品传送的麻烦！！")
-
---核心调用，
+-----------------------------------------------------------
+-- 检测身边是否有 NPC
+-----------------------------------------------------------
 function checkNPCDst()
 	local Obj = Enum2XAllObj()
 	for i = 1, #Obj do
@@ -17,6 +26,10 @@ function checkNPCDst()
 	end
 end
 
+
+-----------------------------------------------------------
+-- 核心调用
+-----------------------------------------------------------
 if CityToGuild() then
    checkNPCDst()
 end
