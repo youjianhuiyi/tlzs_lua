bc01c2cbebc289980a52775ecbe9b93d9f2f6969
yulinzhihou <luolu@qq.com>
2020-07-21 23:42:32 +0800

    7-21 完成自动帮派物品整理

--- userdefine/script/自定义/[功]自动银行整理.lua
+++ userdefine/script/自定义/[功]自动银行整理.lua
@@ -9,13 +9,20 @@
 LoadScript("LtoGuild.lua")
 ---------------------------------------------------------------------
 -- 定义好需要整理的物品，
+-- bag1Items 是道具栏物品
+-- bag2Items 是材料栏物品
 ---------------------------------------------------------------------
 local bag1Items = {
-
+    "低级珍兽还童天书","中级珍兽还童天书","高级珍兽还童天书","超级珍兽还童天书","高级珍兽还童卷轴","低级宝石合成符","宝石雕琢符3级",
+    "龙纹玉灵","缀龙石·元","缀龙石·伤","缀龙石·暴","玉龙髓","净云水","铸纹血玉","玉华灵泉","释灵浆",
 };
 
 local bag2Items = {
-
+    "绿晶石(1级)","蓝晶石(1级)","黄晶石(1级)","红晶石(1级)","猫眼石(1级)",
+    "虎眼石(1级)","石榴石(1级)","月光石(1级)","尖晶石(1级)","祖母绿(1级)",
+    "变石(1级)","黄玉(1级)","碧玺(1级)","紫玉(1级)","皓石(1级)",
+    "黄宝石(1级)","蓝宝石(1级)","绿宝石(1级)","黑宝石(1级)","红宝石(1级)",
+    "丝线3级","染料3级","麟木箭","丹青","彩虹之箭","九宫盒","精铁碎片","棉布碎片","秘银碎片"
 };
 
 ---------------------------------------------------------------------
@@ -57,8 +64,16 @@ end
 -- 先将帮派银行里面指定物品取出，这里只能盲取，所以规定好整理的物品放哪几页很重要
 ---------------------------------------------------------------------
 function getItemsFromGuildBank()
-    
-end
+    for i=1,20 do
+        GetLuaValue("setmetatable(_G, {__index = Banghui_Bank_Env});Banghui_Bank_Item".. i ..":DoAction();","",0);
+        Sleep(50)
+    end
+    GetLuaValue("setmetatable(_G, {__index = MainMenuBar_EnvMainMenuBar_Packet_Clicked();","",0);Sleep(50)
+    GetLuaValue("setmetatable(_G, {__index = Packet_EnvPacket_ChangeTabIndex(0);","",0);Sleep(50)
+    GetLuaValue("setmetatable(_G, {__index = Packet_Env});Packet_PackUp_Clicked();","",0);Sleep(50)
+    GetLuaValue("setmetatable(_G, {__index = Packet_EnvPacket_ChangeTabIndex(1);","",0);Sleep(50)
+    GetLuaValue("setmetatable(_G, {__index = Packet_Env});Packet_PackUp_Clicked();","",0);Sleep(50)
+end 
 
 ---------------------------------------------------------------------
 -- 获取道具栏背包里面的物品并且往对应格子里面扔物品
@@ -95,17 +110,33 @@ end
 ---------------------------------------------------------------------
 -- 判断身边有没有一个帮的人员
 ---------------------------------------------------------------------
-function judgementGuildUsersIsNear( ... )
-    
-    
-
-
+function judgementGuildUsersIsNear()
+    --第一队
+	--遍历周围所有对象数组,有以下属性
+	--id,name ,class ,title,x ,y ,dst ,hp ,guid ,owner ,menpai ,petzrid ,state ,zwType,level ,target ,model ,bhid ,inteam ,leader
+	--id,名称，类型，称号，x坐标，y坐标，距离，血，GUID,怪物拥有者id，门派，珍兽拥有者id,状态，作物类型，等级，目标id,模型id，帮会id,是否在队伍，是否队长
+	local tObj = Enum2XAllObj()
+	--过滤类型。 参数2：依次代表 {NPC,怪物，玩家，珍兽，宝箱} 0排除，1保留，
+	tObj = ObjFilterByClass(tObj, {0, 0, 1, 0, 0})
+	--过滤帮会id,返回帮会id为84的对象
+	tObj = ObjFilterByBHID(tObj, " 52 ")
+	for i = 1, #tObj do
+		local tmp = tObj[i]
+		--取角色名称。对比上面队伍名称,如果是团长号所在的队，不需要做操作，如果不是团队所在的队，需要退队。
+		--将对应需要申请进团的队伍名称转换成字符串进行查找，人员有没有到齐
+		local masterStr = table.concat(allPlayers, ", ", key1, key2 )
+		if string.find( masterStr,tmp.name) ~= nil then
+            return false
+        else
+            return true;
+		end
+	end
 end
 
 ---------------------------------------------------------------------
 -- 往银行扔需要整理的物品
 ---------------------------------------------------------------------
-function throwFixUpItems()
+function throwFixUpItems(startIndex,endIndex)
 	--判断是否在银行NPC附近。判断是否打开
 	--扔完20次之后再切换到下一个箱子
 	--如果打开则每次打开扔20次，不管有没有空间。
@@ -114,14 +145,18 @@ function throwFixUpItems()
 	--无限循环下去，直到取不到图则停止，执行收图操作。
 	--收图操作，固定收图120张之后直接 停止 收图。直接 往银行扔图
 	-- 放图到第4个-第5个箱
-	for i =4,5 do
-		checkNPCDst()
-		GetLuaValue("setmetatable(_G, {__index = Banghui_Bank_Env});Banghui_Bank_patulousBox_Clicked(".. i ..");","",0)
-		Sleep(100)
-		findBaseItemsToBank()
-		if checkBag() then
-			break
-		end
+	for i = startIndex,endIndex do
+        checkNPCDst()
+        if judgementGuildUsersIsNear() then
+            GetLuaValue("setmetatable(_G, {__index = Banghui_Bank_Env});Banghui_Bank_patulousBox_Clicked(".. i ..");","",0)
+            Sleep(100)
+            -- 先从一页里面取出所有物品
+            getItemsFromGuildBank();
+            -- 然后再根据指定物品往里面扔
+            findBaseItemsToBank()
+        else
+            PushDebugMessage("#eDC4C18#cFFFF00 请耐心等待其他大佬操作完成再试！！");
+        end
 	end
 end
 
@@ -130,12 +165,21 @@ end
 -- 检查背包是否还有图
 ---------------------------------------------------------------------
 function checkBag()
-	local nfindIdx = Bag:FindBagItem_1("藏宝图")
-	if nfindIdx ~= -1 then
-		-- 一直往银行扔图
-		return false
-	end
-	return true
+    local allBagsItems = table.concat( bag1Items,bag2Items);
+    
+    for k,v in ipairs(allBagsItems) do
+       --查找道具+材料+任务,会叠加统计所有找到的数量 (3个返回值)
+        --	返回值：3个(逻辑型 整数型 整数型) 是否找到 所有数量 位置索引
+        --  参数1：文本型		物品名称，只支持精确名字
+        local bFind, nCount, nIndex = Bag:FindBagItem_All(v);
+        if bFind then
+            -- 表示背包里面有没有存完的物品，可能是里面的物品已经满了，这个时候需要随机往前面扔
+            return false;
+        else
+            -- 表示背包里面的物品已经全部存进去了。可以走了
+            return true;
+        end
+    end
 end
 
 
@@ -144,18 +188,24 @@ end
 ---------------------------------------------------------------------
 while true do
 	-- 检测背包
-	if checkBag() == true then 
-		break
+    if checkBag() == true then 
+        PushDebugMessage("#eDC4C18#cFFFF00 已经整理完成，可以闪人了");
+        LoadScript("[新]回城");
+		break;
 	else
-		PushDebugMessage("准备去帮会")
+		PushDebugMessage("#eDC4C18#cFFFF00 准备去帮会")
 		toGuild()
-		PushDebugMessage("准备循环放图到银行，直到全部放完")
-		while true do
-			if checkBag() ~= true then
-				throwFixUpItems()
-			else
+        PushDebugMessage("#eDC4C18#cFFFF00 准备循环放图到银行，直到全部放完");
+        -- 循环 5 次存，如果第三次还没存完，尝试其他格子存仓库
+        for i=1,2 do
+			if checkBag() ~= true and i == 1 then
+				throwFixUpItems(4,5)
+            elseif checkBag() ~= true and i == 2 then 
+                -- 如果第2次还没存完，表示肯定是指定格子满了
+                throwFixUpItems(1,4)
+            else
 				break
-			end
+            end
 		end
 	end	
 end
