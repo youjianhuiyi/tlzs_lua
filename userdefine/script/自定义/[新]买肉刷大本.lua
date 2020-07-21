adc8d53592057fea5de68d1570c4190303c0e18a
yulinzhihou <luolu@qq.com>
2020-07-21 15:34:36 +0800

    7-21 完成最大肉购买的调整

--- userdefine/script/自定义/[新]买肉刷大本.lua
+++ userdefine/script/自定义/[新]买肉刷大本.lua
@@ -5,7 +5,7 @@
 	github:https://github.com/yulinzhihou
 	Date:2020-07-21
 ]]
-
+local DropCommonItem = {"后肘肉口粮","蝗虫口粮","棕榈口粮"}
 
 -----------------------------------------------------------
 -- 去楼兰宝宝 NPC 处
@@ -26,51 +26,59 @@ function checkNPCDst()
 end
 
 -----------------------------------------------------------
--- 自动买最大肉
+-- 判断背包里面的脚本
 -----------------------------------------------------------
 function judgeBagIsEmpty()
-    
+    local tObj = Bag:EnumAllObj()
+	local n = 0
+	local m = 0
+	for i = 1, #tObj do
+		local tmp = tObj[i]
+		if tmp.index <= 29 then 
+			n = n + 1
+		elseif tmp.index > 29 and tmp.index < 60 then 
+			m = m + 1
+		end
+	end
+	return {bag1 = n,bag2 = m}
 end
-
-
 
 -----------------------------------------------------------
 -- 自动买最大肉
 -----------------------------------------------------------
 function buyPetEats()
-    if condition then
+    local bags = judgeBagIsEmpty();
+    if bags[bag1] ~= 29  then
         Packet_PackUp_Clicked();--整理背包
-    end
-    MoveToNPC(109,121,246,"云兮兮");Sleep(500)
-    QuestFrameOptionClicked("看看你卖的东西");Sleep(500)
-    PushDebugMessage("#eDC4C18#cFFFF00买指定数量的肉与快乐度物品，准备刷新副本")
-    for i = 1,2 do
-        --下一页
-        GetLuaValue("setmetatable(_G, {__index = Shop_Env});Booth_PageDown();","",0);Sleep(500);
+        MoveToNPC(109,121,246,"云兮兮");Sleep(500)
+        QuestFrameOptionClicked("看看你卖的东西");Sleep(500)
+        PushDebugMessage("#eDC4C18#cFFFF00买指定数量的肉与快乐度物品，准备刷新副本")
+        for i = 1,2 do
+            --下一页
+            GetLuaValue("setmetatable(_G, {__index = Shop_Env});Booth_PageDown();","",0);Sleep(500);
+            --批量购买
+            GetLuaValue("setmetatable(_G, {__index = Shop_Env});Booth_BuyMult();","",0);Sleep(500);
+            --买后腿肉
+            GetLuaValue("setmetatable(_G, {__index = Shop_Env});GoodButton_Clicked(6);","",0);Sleep(500);
+            --买两次
+            GetLuaValue("setmetatable(_G, {__index = Shop_BulkBuyingEx_Env});Shop_BulkBuying_Accept_Ex_Clicked();","",0);Sleep(500);
+        end
+        --上一页
+        GetLuaValue("setmetatable(_G, {__index = Shop_Env});Booth_PageUp();","",0);Sleep(500);
         --批量购买
         GetLuaValue("setmetatable(_G, {__index = Shop_Env});Booth_BuyMult();","",0);Sleep(500);
         --买后腿肉
-        GetLuaValue("setmetatable(_G, {__index = Shop_Env});GoodButton_Clicked(6);","",0);Sleep(500);
-        --买两次
+        GetLuaValue("setmetatable(_G, {__index = Shop_Env});GoodButton_Clicked(4);","",0);Sleep(500);
+        --买最大
+        GetLuaValue("setmetatable(_G, {__index = Shop_BulkBuyingEx_Env});Shop_BulkBuying_Max_Ex_Clicked();","",0);Sleep(500);
+        GetLuaValue("setmetatable(_G, {__index = Shop_BulkBuyingEx_Env});Shop_BulkBuying_Max_Ex_Clicked();","",0);Sleep(200)
         GetLuaValue("setmetatable(_G, {__index = Shop_BulkBuyingEx_Env});Shop_BulkBuying_Accept_Ex_Clicked();","",0);Sleep(500);
     end
-    --上一页
-    GetLuaValue("setmetatable(_G, {__index = Shop_Env});Booth_PageUp();","",0);Sleep(500);
-    --批量购买
-    GetLuaValue("setmetatable(_G, {__index = Shop_Env});Booth_BuyMult();","",0);Sleep(500);
-    --买后腿肉
-    GetLuaValue("setmetatable(_G, {__index = Shop_Env});GoodButton_Clicked(4);","",0);Sleep(500);
-    --买最大
-    GetLuaValue("setmetatable(_G, {__index = Shop_BulkBuyingEx_Env});Shop_BulkBuying_Max_Ex_Clicked();","",0);Sleep(500);
-    GetLuaValue("setmetatable(_G, {__index = Shop_BulkBuyingEx_Env});Shop_BulkBuying_Max_Ex_Clicked();","",0);Sleep(200)
-    GetLuaValue("setmetatable(_G, {__index = Shop_BulkBuyingEx_Env});Shop_BulkBuying_Accept_Ex_Clicked();","",0);Sleep(500);
     PushDebugMessage("#eDC4C18#cFFFF00 买肉结束，准备开刷")
 end
 
-local DropCommonItem = {"后肘肉口粮","蝗虫口粮","棕榈口粮"}
-
 -----------------------------------------------------------
--- 自动回城
+-- 买满肉需要销毁一组
 -----------------------------------------------------------
 function destroyItem()
 	for key,value in ipairs(DropCommonItem) do
@@ -79,7 +87,7 @@ function destroyItem()
 			PushDebugMessage("销毁物品名称： [" .. value.."]");
 			-- 等待(50)
 			CallFun(5,nIndex);
-			Sleep(10);
+			Sleep(50);
 		end
 	end
 	
@@ -87,22 +95,12 @@ end
 
 
 -----------------------------------------------------------
--- 自动回城
+-- 核心调用
 -----------------------------------------------------------
---核心调用
-for i = 1,60 do
-	PushDebugMessage("#cff0000 防掉线检测,剩余： [" .. i.."] 秒后执行！！！");
-	if i == 60 then 
-		break
-	end
-	Sleep(1000)
-end
-
 checkNPCDst()
 buyPetEats()
-
 --下面是销毁6次，物品名写在下面
 --大概5秒钟销毁一次
-for i = 1,4 do
+for i = 1,3 do
 	destroyItem();
 end
--- userdefine/script/自定义/[新]日常交易物品整合.lua
+++ userdefine/script/自定义/[新]日常交易物品整合.lua
@@ -14,13 +14,13 @@ local players = {
 
 function main()
 	-- 第一步,先销毁背包里面的宝宝肉
-	执行脚本("[功]销指定物品")
+	LoadScript("[功]销指定物品")
 	-- 第二步，先取出天机锦囊里面的所有需要取出的物品
-	执行脚本("锦囊取物")
+	LoadScript("锦囊取物")
 	-- 第三步，将取出来的物品存仓一次
-	执行脚本("[功能] 物品存仓")
+	LoadScript("[功能] 物品存仓")
 	-- 第四步，将取出来的物品存仓一次
-	执行脚本("加工材料")
+	LoadScript("加工材料")
 	local g_szItemName = AI_GetParameter("仓库取物")
 	--参数1：需取出的物品名称 ，空格分开
 	--参数2：取出的数量
@@ -29,7 +29,7 @@ function main()
 	--参数5：需取出的锁定状态 0无所谓 1不绑的 2绑定的 可空
 	local nCnt = Bank_GetItem(items, 31, 1, true)
 	for i = 1 , 4 do
-		执行脚本("交易给老板号");Sleep(100);
+		LoadScript("交易给老板号");Sleep(100);
 	end
 end
 -- 执行主控
